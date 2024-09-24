9c6350ee66562b3cd94ac2c8d45dd62100cf5b6f
Riya Sinha <sinha2310riya@gmail.com>
2024-09-25 01:41:48 +0530

    merged changes

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -6,7 +6,7 @@ const AgePage = async() => {
   return (
     <div className="h-full max-w-[912px] px-3 mx-auto">
       <h1 className="text-3xl font-bold text-blue-500 text-center mb-8">
-        Choose Your Learning Path
+        Create Your Learning Path
       </h1>
       <ListPage />
     </div>
