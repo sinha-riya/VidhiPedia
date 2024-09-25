42d08964539de0b336e676a666b53e7362c49642
jiyadd <mjiyad119@gmail.com>
2024-09-25 18:10:25 +0530

    Update page.tsx

--- app/(main)/adult/learn/page.tsx
+++ app/(main)/adult/learn/page.tsx
@@ -6,7 +6,7 @@ const AgePage = () => {
   return (
     <div className="h-full px-3 mx-auto">
       <iframe
-        src="https://roadmap.sh/r/embed?id=66ec8454b32bbae9e23568df"
+        src="https://roadmap.sh/r/embed?id=66f3f101c45e253cb04d9e9d"
         width="100%"
         height="100%"
         frameBorder="0"
